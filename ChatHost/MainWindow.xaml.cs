9f724071193aad3a8f22a57fa9e31bb410a15f4f
Michael Drejer Andersen <mich450m@student.eadania.dk>
2018-10-31 12:35:48 +0100

    asswaht?

--- ChatHost/MainWindow.xaml.cs
+++ ChatHost/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ChatProgram;
 
 namespace ChatHost
 {
@@ -23,6 +26,34 @@ namespace ChatHost
         public MainWindow()
         {
             InitializeComponent();
+            // Step 1 of the address configuration procedure: Create a URI to serve as the base address.
+            Uri baseAddress = new Uri("http://localhost:8000/ChatProgram/ChatService");
+
+            // Step 2 of the hosting procedure: Create ServiceHost
+            ServiceHost selfHost = new ServiceHost(typeof(ChatService), baseAddress);
+
+            try
+            {
+                // Step 3 of the hosting procedure: Add a service endpoint.
+                selfHost.AddServiceEndpoint(typeof(IChatService), new WSHttpBinding(), "ChatService");
+
+                // Step 4 of the hosting procedure: Enable metadata exchange.
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                selfHost.Description.Behaviors.Add(smb);
+
+                // Step 5 of the hosting procedure: Start (and then stop) the service.
+                selfHost.Open();
+                Console.WriteLine("heeey");
+
+                // Close the ServiceHostBase to shutdown the service.
+                selfHost.Close();
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine("An exception occurred: {0}", ce.Message);
+                selfHost.Abort();
+            }
         }
     }
 }
