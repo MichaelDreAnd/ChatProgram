e156ee4469ad8bf2d0d433eec2a2d5f9a741699a
nick3925 <nick3925@student.eadania.dk>
2018-11-02 11:06:02 +0100

    Localhost changed.

--- ChatProgram/ChatService.cs
+++ ChatProgram/ChatService.cs
@@ -12,20 +12,9 @@ using ChatProgram;
 namespace ChatProgram
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ChatService" in both code and config file together.
-    public class ChatService : IChatService, INotifyPropertyChanged
-
-       
-
+    public class ChatService : IChatService
     {
-
         Room room = new Room();
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
 
         public void Connect(string UserName, string IpAdress)
         {
@@ -45,8 +34,6 @@ namespace ChatProgram
         public void SendMessage(string message)
         {
             room.RoomString += message + "\n";
-            NotifyPropertyChanged();
-
         }
 
         
